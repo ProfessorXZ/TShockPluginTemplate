a3a2fb70688fb3bd24536190c8aea279d863ce73
ProfessorXZ <ssbiljan@gmail.com>
2017-09-15 23:58:16 +0200

    Added project files

---
+++ TShockPluginTemplate/TShockPluginTemplatePlugin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+using Terraria;
+using TerrariaApi.Server;
+
+namespace TShockPluginTemplate
+{
+    /// <summary>
+    /// Represents the TShockPluginTemplate plugin.
+    /// </summary>
+    [ApiVersion(2, 1)]
+    public class TShockPluginTemplatePlugin : TerrariaPlugin
+    {
+        /// <inheritdoc />
+        public TShockPluginTemplatePlugin(Main game) : base(game)
+        {
+        }
+
+        /// <inheritdoc />
+        public override string Author => "Plugin author";
+
+        /// <inheritdoc />
+        public override string Description => "Plugin description";
+        
+        /// <inheritdoc />
+        public override string Name => "Plugin name";
+
+        /// <inheritdoc />
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+        /// <inheritdoc />
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                
+            }
+            
+            base.Dispose(disposing);
+        }
+
+        /// <inheritdoc />
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
